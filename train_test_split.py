9c9e6cf84a5c61a680b2ec1a7678512f259730ff
dinupa1 <dinupa3@gmail.com>
2024-05-16 01:50:57 -0500

    minor bug fix

--- train_test_split.py
+++ train_test_split.py
@@ -7,19 +7,13 @@ import awkward as ak
 from sklearn.model_selection import train_test_split
 
 
-tree_P = uproot.open("/seaquest/users/knagai/Public/data/E906/simple_str/4pi/67_LH2_4pi/data.root:save")
+tree_P = uproot.open("small.root:save")
 tree_D = uproot.open("/seaquest/users/knagai/Public/data/E906/simple_str/mc/LH2_run3_messy_1201_2398/data.root:save")
 
-
-events_P = tree_P.arrays(["mass", "pT", "x1", "x2", "xF", "phi", "costh", "occuD1"])
+events_P = tree_P.arrays(["mass", "pT", "x1", "x2", "xF", "phi", "costh"])
 events_D = tree_D.arrays(["mass", "pT", "xF", "phi", "costh", "true_phi", "true_costh", "occuD1", "true_mass"])
 
-events_P1 = events_P[(events_P.mass > 5.5) & (events_P.occuD1 < 300.)].to_numpy()
-
-
-data_P0, data_P1 = train_test_split(events_P1, test_size=0.5, shuffle=True)
-
-data_train_val_P, data_test_fit_P = train_test_split(data_P0, test_size=0.5, shuffle=True)
+data_train_val_P, data_test_fit_P = train_test_split(events_P, test_size=0.5, shuffle=True)
 data_train_P, data_val_P = train_test_split(data_train_val_P, test_size=0.5, shuffle=True)
 data_test_P, data_fit_P = train_test_split(data_test_fit_P, test_size=0.5, shuffle=True)
 
@@ -63,7 +57,9 @@ fit_dic_P = {
     "costh": data_fit_P["costh"],
 }
 
-events_D1 = events_D[(events_D.true_mass > 5.5) & (events_D.occuD1 < 300.)].to_numpy()
+print(data_fit_P["mass"].shape)
+
+events_D1 = events_D[(events_D.true_mass > 4.5) & (events_D.occuD1 < 300.)].to_numpy()
 
 data_train_val_D, data_test_fit_D = train_test_split(events_D1, test_size=0.5, shuffle=True)
 data_train_D, data_val_D = train_test_split(data_train_val_D, test_size=0.5, shuffle=True)
