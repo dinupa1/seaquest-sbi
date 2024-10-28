9dcbc133010579dcec2a1abfdd90a53fac4a4a13
Dinupa Nawarathne <dinupa3@gmail.com>
2024-10-28 15:26:38 -0500

    3D simulation added

--- data_augmentation.cc
+++ data_augmentation.cc
@@ -27,8 +27,9 @@ void data_augmentation() {
     gSystem->Exec("python ./simulators/generator.py");
 
     auto infile = TFile::Open("./data/generator.root", "read");
-    auto train = (TTree*)infile->Get("train");
-    auto test = (TTree*)infile->Get("test");
+    auto X_train = (TTree*)infile->Get("X_train");
+    auto X_val = (TTree*)infile->Get("X_val");
+    auto X_test = (TTree*)infile->Get("X_test");
 
     auto outfile = new TFile("./data/outputs.root", "recreate");
 
@@ -47,12 +48,16 @@ void data_augmentation() {
     }
 
     auto sim1 = new simulator2D("train_tree");
-    sim1->samples(train, generator, N_train, N_data);
+    sim1->samples(X_train, generator, N_train, N_data);
     sim1->save();
 
-    auto sim2 = new simulator2D("test_tree");
-    sim2->samples(test, generator, N_test, N_data);
+    auto sim2 = new simulator2D("val_tree");
+    sim2->samples(X_val, generator, N_train, N_data);
     sim2->save();
+
+    auto sim3 = new simulator2D("test_tree");
+    sim3->samples(X_test, generator, N_test, N_data);
+    sim3->save();
 
     outfile->Write();
     outfile->Close();
