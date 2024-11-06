2c6f4cf73243656f1c9f6ad5960f4b3a354b42a3
Dinupa Nawarathne <dinupa3@gmail.com>
2024-11-06 01:57:40 -0600

    index miss match

--- simulators/generator.py
+++ simulators/generator.py
@@ -10,9 +10,9 @@ seed: int=42
 
 np.random.seed(seed)
 
-n_data:int = 2000000
-n_theta:int = 12
-n_prior:int = 15000
+n_data:int = 1000000
+n_theta:int = 3
+n_prior:int = 3750
 
 print("[ ===> train test split ]")
 
@@ -20,18 +20,21 @@ save = uproot.open("./data/data.root:save")
 branches = ["mass", "pT", "xF", "phi", "costh", "true_mass", "true_pT", "true_xF", "true_phi", "true_costh", "occuD1"]
 events = save.arrays(branches)
 
-events1 = events[(events.mass > 4.5) & (events.mass < 9.) & (events.xF > 0.) & (events.xF < 1.) & (np.abs(events.costh) < 0.4) & (events.occuD1 < 300.)]
+#
+# pT = {0., 0.43, 0.67, 0.96, 2.5}
+#
+
+events1 = events[(events.mass > 4.5) & (events.mass < 9.) & (events.xF > 0.) & (events.xF < 1.) & (np.abs(events.costh) < 0.4) & (events.occuD1 < 300.) & (0. < events.pT) & (events.pT < 0.43)]
 
 X_train_val, X_test = train_test_split(events1.to_numpy(), test_size=0.3, shuffle=True)
 X_train, X_val = train_test_split(X_train_val, test_size=0.5, shuffle=True)
 
-theta = np.random.uniform([-1.5, -0.6, -0.6, -1.5, -0.6, -0.6, -1.5, -0.6, -0.6, -1.5, -0.6, -0.6], [1.5, 0.6, 0.6, 1.5, 0.6, 0.6, 1.5, 0.6, 0.6, 1.5, 0.6, 0.6], [n_data, n_theta])
+theta = np.random.uniform([-1.5, -0.6, -0.6], [1.5, 0.6, 0.6], [n_data, n_theta])
 
-theta_train_test, theta_prior = train_test_split(theta, test_size=n_prior, shuffle=True)
+theta_train_val, theta_test = train_test_split(theta, test_size=0.1, shuffle=True)
+theta_train, theta_val = train_test_split(theta_train_val, test_size=0.3, shuffle=True)
 
-theta_train_val, theta_test = train_test_split(theta_train_test, test_size=0.2, shuffle=True)
-theta_train, theta_val = train_test_split(theta_train_val, test_size=0.25, shuffle=True)
-
+theta_test, theta_prior = train_test_split(theta_test, test_size=n_prior, shuffle=True)
 theta0_val, theta1_val = train_test_split(theta_val, test_size=0.5, shuffle=True)
 theta0_train, theta1_train = train_test_split(theta_train, test_size=0.5, shuffle=True)
 
